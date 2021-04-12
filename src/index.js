3150da4527fc4e69658650ee9bbd85ff88a88c51
Philippe Timothe <PhilippeTimothe@gmail.com>
2021-04-12 15:57:37 -0400

    Implementation of dynamic creation of table at the moment page is loaded

--- src/index.js
+++ src/index.js
@@ -5,14 +5,46 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     const mainDiv = document.querySelector(".main_div")
-
-    mainDiv.addEventListener("click", function() {
-        const target = event.target
-        if (target === event.currentTarget) {
-            // do something here...
-        }
-    })
-
     
+    function createTable() {
+        const table = document.createElement("table");
+        table.className = "table table-hover";
+        table.style = "width:60%;";
+
+        const tableBody = document.createElement("tbody");
+        tableBody.style = "text-align: center;";
+        tableBody.innerHTML = `
+            <tr>
+                <th class="text-center">Firstname</th>
+                <th class="text-center">Lastname</th> 
+                <th class="text-center">Email</th>
+            </tr>
+            <tr>
+                <td>Jill</td>
+                <td>Smith</td>
+                <td>JillSmith@gmail.com</td>
+            </tr>
+        `;
+
+        const addButton = document.createElement('button');
+        addButton.className = "btn btn-default";
+        addButton.type = "submit";
+        addButton.innerHTML = "Add Contact";
+
+        table.append(tableBody);
+        mainDiv.append(table);
+        mainDiv.append(addButton);
+    }
+
+    createTable()
+
+    // mainDiv.addEventListener("click", function() {
+    //     const target = event.target
+    //     if (target === event.currentTarget) {
+    //         // do something here...
+    //     }
+    // })
+
+
 });
 
